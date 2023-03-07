3986c80dad3ca7bedeb937f2af7d6b5e06c7a0e1
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-03-07 03:25:56 +0300

    feat: add notification if the room has both players

---
+++ src/components/Notification.tsx
@@ -0,0 +1,52 @@
+import React, { memo, useEffect, useRef, useState } from 'react';
+import { Alert } from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
+
+interface NotificationProps {
+  closeNotification: () => void;
+  isShown: boolean;
+}
+
+function Notification({ closeNotification, isShown }: NotificationProps) {
+  const [mountNotification, setMountNotification] = useState(false);
+  const alertRef = useRef(null);
+  useEffect(() => {
+    if (isShown) {
+      setMountNotification(true);
+      setTimeout(() => setMountNotification(false), 2200);
+    }
+  }, [isShown]);
+
+  useEffect(() => {
+    if (!mountNotification) {
+      setTimeout(() => closeNotification(), 200);
+    }
+  }, [mountNotification]);
+
+  return (
+    <CSSTransition
+      nodeRef={alertRef}
+      in={mountNotification}
+      timeout={200}
+      classNames="alert"
+    >
+      <div>
+        {isShown && (
+          <Alert
+            ref={alertRef}
+            variant="primary"
+            onClose={() => setMountNotification(false)}
+            dismissible
+          >
+            <p>
+              Sorry, selected room already has both players, choose another room
+              or try again later.
+            </p>
+          </Alert>
+        )}
+      </div>
+    </CSSTransition>
+  );
+}
+
+export default memo(Notification);
--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
+
+import Notification from 'components/Notification';
 
 import LoginForm from '../components/LoginForm';
 import UserData from '../utils/interfaces';
@@ -12,6 +14,8 @@ interface StartPageProps {
 }
 
 function StartPage({ setCurrentUser, setUsers, users }: StartPageProps) {
+  const [isNotificationShown, setNotificationShown] = useState(false);
+
   if (users.length) {
     return <Navigate to="/game" />;
   }
@@ -20,6 +24,10 @@ function StartPage({ setCurrentUser, setUsers, users }: StartPageProps) {
       <p className="mb--1 text-center">{`Let's play`}</p>
       <p className="text-center">tic-tac-toe</p>
       <LoginForm setCurrentUser={setCurrentUser} setUsers={setUsers} />
+      <Notification
+        isShown={isNotificationShown}
+        closeNotification={() => setNotificationShown(false)}
+      />
     </div>
   );
 }
